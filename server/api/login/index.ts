772b2f519e39f5a93997bcba109caaf72ba77310
The Witcher <82153670+TheWTCHR@users.noreply.github.com>
2022-12-31 17:39:27 +0300

    Update index.ts

--- server/api/login/index.ts
+++ server/api/login/index.ts
@@ -3,7 +3,7 @@ import { saveRegister, getRegister } from "~~/utils/database.js";
 export default defineEventHandler(async (event) => {
     const query = getQuery(event);
     const header = getHeader(event, "Auth");
-    const reg = await (await getRegister(query.mail)).rows;
+    const reg = (await getRegister(query.mail)).rows;
     if(header != "witcherapikey") return "Invalid API Key";
     if(query.mail && query.pass && query.mail.includes("@") && query.mail.includes(".") && reg[0].password === query.pass && reg.length > 0) {
         return true;
